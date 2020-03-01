42672d9f6c57d1707bfa4310e2fa3f3985bbf44f
stasisha <stasisha@users.noreply.github.com>
2020-03-01 23:00:21 +0200

    Update file-edit.sh

--- file-edit.sh
+++ file-edit.sh
@@ -5,17 +5,3 @@ addLineToBottomIfNotExists() {
   local FILE=$2
   sudo grep -qF -- "$LINE" "$FILE" || echo "$LINE" | sudo tee -a "$FILE"
 }
-
-addLineToTopIfNotExists() {
-  local LINE=$1
-  local FILE=$2
-  sudo grep -qF -- "$LINE" "$FILE" || echo -e  "$LINE""\n$(cat todo.txt)" > todo.txt
-}
-
-addLineBeforeLineIfNotExists() {
-  local LINE=$1
-  local BEFORE=$2
-  local FILE=$3
-  sudo grep -qF -- "$LINE" "$FILE" || sed  "/${BEFORE}/ { N; s/cat\n/${LINE}\n&/ }" "$FILE"
-}
-
