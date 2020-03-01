0e7847f27a4e5ee4c665d1666a3ead77712407ec
stasisha <stasisha@users.noreply.github.com>
2020-03-01 23:00:30 +0200

    Update file-edit.sh

--- file-edit.sh
+++ file-edit.sh
@@ -3,5 +3,5 @@
 addLineToBottomIfNotExists() {
   local LINE=$1
   local FILE=$2
-  sudo grep -qF -- "$LINE" "$FILE" || echo "$LINE" | sudo tee -a "$FILE"
+  grep -qF -- "$LINE" "$FILE" || echo "$LINE" | sudo tee -a "$FILE"
 }
